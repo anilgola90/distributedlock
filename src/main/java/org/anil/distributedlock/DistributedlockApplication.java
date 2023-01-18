9c0faeb10d5a4117cc9d892b1fd7f67239a680ad
Anil Gola <anil.gola@tothenew.com>
2023-01-18 22:25:05 +0530

    commit

---
+++ src/main/java/org/anil/distributedlock/DistributedlockApplication.java
@@ -0,0 +1,13 @@
+package org.anil.distributedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedlockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/anil/distributedlock/DistributedlockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anil.distributedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedlockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
